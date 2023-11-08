c13aaa71bf250b52d1c8367117f6f7eb36414718
Dharsan15 <dharsanravi6677@gmail.com>
2023-11-08 23:26:24 +0530

    new commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,11 +3,11 @@ import { restaurantList } from "../../utils/constants";
 import CardLayout from "./CardLayout";
 
 const Body = () => (
-    <div className="restro-list">
-      {restaurantList.map((restaurant) => {
-        return <CardLayout {...restaurant.data} />;
-      })}
-    </div>
-  );
+  <div className="restro-list">
+    {restaurantList.map((restaurant) => {
+      return <CardLayout {...restaurant.data} />;
+    })}
+  </div>
+);
 
-  export default Body;
+export default Body;
--- src/components/CardLayout.js
+++ src/components/CardLayout.js
@@ -1,32 +1,30 @@
 const CardLayout = ({
-    cloudinaryImageId,
-    name,
-    cuisines,
-    area,
-    avgRating,
-    costFortwoString,
-  }) => (
-    <div className="restro-card">
-      <div>
-        <img
-          className="food-image"
-          src={
-            "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
-            cloudinaryImageId
-          }
-        />
-      </div>
-      <div className="card-contents">
-        <h3>{name}</h3>
-        <p>{cuisines.join(",")}</p>
-        <div className="card-footer">
+  cloudinaryImageId,
+  name,
+  cuisines,
+  area,
+  avgRating,
+  costFortwoString,
+}) => (
+  <div className="restro-card">
+    <div>
+      <img
+        className="food-image"
+        src={
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
+          cloudinaryImageId
+        }
+      />
+    </div>
+    <div className="card-contents">
+      <h3>{name}</h3>
+      <p>{cuisines.join(",")}</p>
+      <div className="card-footer">
         <p> ⭐ {avgRating} </p>
-          <p> {area} </p>
-        </div>
+        <p> {area} </p>
       </div>
     </div>
-  );
+  </div>
+);
 
-
-  export default CardLayout;
-  
+export default CardLayout;
