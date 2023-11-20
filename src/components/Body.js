a47e6122c90645b328e04ddb6453d9ca2273314c
Dharsan15 <dharsanravi6677@gmail.com>
2023-11-20 22:24:01 +0530

    new commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,29 +3,27 @@ import { restaurantList } from "../../utils/constants";
 
 import CardLayout from "./CardLayout";
 
-
 const Body = () => {
-   
-  const[resList , setrestaurantList] = useState(restaurantList)
+  const [resList, setrestaurantList] = useState(restaurantList);
 
   return (
-  <>
-    <button
-      onClick={() => {
-         const newArr = resList.filter((res) => res.data.avgRating >= 4.2);
-        setrestaurantList(newArr)
-      }}
-    >
-      Top rated restaurants
-    </button>
+    <>
+      <button
+        onClick={() => {
+          const newArr = resList.filter((res) => res.data.avgRating >= 4.2);
+          setrestaurantList(newArr);
+        }}
+      >
+        Top rated restaurants
+      </button>
 
-    <div className="restro-list">
-      {resList.map((restaurant) => {
-        return <CardLayout {...restaurant.data} />;
-      })}
-    </div>
-  </>
-  )
+      <div className="restro-list">
+        {resList.map((restaurant) => {
+          return <CardLayout {...restaurant.data} />;
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Body;
